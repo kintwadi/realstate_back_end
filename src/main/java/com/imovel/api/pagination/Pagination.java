eea244084543771e65aee96258917558524145eb
kintwadi <joinemabi@gmail.com>
2025-08-02 21:47:42 +0200

    base pagination

---
+++ src/main/java/com/imovel/api/pagination/Pagination.java
@@ -0,0 +1,30 @@
+package com.imovel.api.pagination;
+
+public class Pagination
+{
+    
+    private int pageSize;
+    private int pageNumber;
+ 
+    public int getPageSize()
+    {
+        return pageSize;
+    }
+
+    public void setPageSize( int pageSize )
+    {
+        this.pageSize = pageSize;
+    }
+
+    public int getPageNumber()
+    {
+        return pageNumber;
+    }
+
+    public void setPageNumber( int pageNumber )
+    {
+        this.pageNumber = pageNumber;
+    }
+
+
+}
